1f934a070be523799c139d6fabef6ddfb2d02628
Cory DeGarmo <degarmo@gmail.com>
2017-01-09 08:37:55 -0600

    static pages

---
+++ app/controllers/p_controller.rb
@@ -0,0 +1,31 @@
+class PController < ApplicationController
+  def index
+  end
+
+  def about
+  end
+
+  def solutions
+  end
+
+  def customers
+  end
+
+  def careers
+  end
+
+  def specialized_training
+  end
+
+  def info_sys
+  end
+
+  def languages
+  end
+
+  def medical
+  end
+
+  def staffing
+  end
+end
--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,25 @@
 Rails.application.routes.draw do
+  get 'p/index'
+
+  get 'p/about'
+
+  get 'p/solutions'
+
+  get 'p/customers'
+
+  get 'p/careers'
+
+  get 'p/specialized_training'
+
+  get 'p/info_sys'
+
+  get 'p/languages'
+
+  get 'p/medical'
+
+  get 'p/staffing'
+
+  root to: 'p#index'
+
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
