bb28421bdf1713daca90f50dd66c4ff85eaf6656
Cory DeGarmo <degarmo@gmail.com>
2017-01-16 19:04:37 -0600

    updated job form

--- app/controllers/jobs_controller.rb
+++ app/controllers/jobs_controller.rb
@@ -1,60 +1,57 @@
 class JobsController < ApplicationController
-	before_action :require_login, only: [:edit, :create, :new]
+  	before_action :require_login, only: [:edit, :create, :new]
 
-	
-  def index
-  	@job = Job.page(params[:page]).per(10).order(created_at: :desc)
-  end
-
-  def new
-  	@job = Job.new
-  end
-
-  def archive
-
-  end
-
-  def create
-  	@job = Job.new(params.require(:job).permit(:title, :company, :pay, :location, :description))
-  	if @job.save
-  		redirect_to jobs_path
-  	else
-  		render "new"
-  	end
-  end
-
-  def show
-    @job = Job.find(params[:id])
-  end
-
-  def edit
-    @job = Job.find(params[:id])
-  end
-
-  def update
-    @job = Job.find(params[:id])
-    if @job.update(params.require(:job).permit(:title, :company, :pay, :location, :description))
-       flash[:notice] = "Successfully update job!"
-      redirect_to jobs_path
-    else
-       flash[:notice] = "Error updating job!"
-      render 'edit'
+  	
+    def index
+    	@job = Job.page(params[:page]).per(10).order(created_at: :desc)
     end
-  end
 
-  def archive
-    def archive        
-    update_attribute!(:displayed, false)
-  end 
+    def new
+    	@job = Job.new
+    end
+
+    def create
+    	@job = Job.new(params.require(:job).permit(:title, :company, :pay, :location, :description))
+    	if @job.save
+    		redirect_to jobs_path
+    	else
+    		render "new"
+    	end
+    end
+
+    def show
+      @job = Job.find(params[:id])
+    end
+
+    def edit
+      @job = Job.find(params[:id])
+    end
+
+    def update
+      @job = Job.find(params[:id])
+      if @job.update(params.require(:job).permit(:title, :company, :pay, :location, :description))
+         flash[:notice] = "Successfully update job!"
+        redirect_to jobs_path
+      else
+         flash[:notice] = "Error updating job!"
+        render 'edit'
+      end
+    end
+
+    def archive
+      def archive        
+      update_attribute!(:displayed, false)
+    end 
 
 
-  private
+    private
 
-  def jobs_params
-    params.require(:title).permit(:title, :company, :pay, :description)
-  end
+    def jobs_params
+      params.require(:title).permit(:title, :company, :pay, :description)
+    end
 
-  def find_jobs
-    @job = Jobs.find(params[:id])
+    def find_jobs
+      @job = Jobs.find(params[:id])
+    end
   end
 end
