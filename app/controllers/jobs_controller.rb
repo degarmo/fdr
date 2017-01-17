cb816eb6c47e90adfb15be417282a6e22a8f44e5
Cory DeGarmo <degarmo@gmail.com>
2017-01-17 14:01:20 -0600

    fixed controllers page

--- app/controllers/jobs_controller.rb
+++ app/controllers/jobs_controller.rb
@@ -1,57 +1,54 @@
 class JobsController < ApplicationController
-  	before_action :require_login, only: [:edit, :create, :new]
+	before_action :require_login, only: [:edit, :create, :new]
+	
+  def index
+    @job = Job.page(params[:page]).per(10).order(created_at: :desc)
+  end
 
-  	
-    def index
-    	@job = Job.page(params[:page]).per(10).order(created_at: :desc)
+  def new
+  	@job = Job.new
+  end
+
+  def create
+  	@job = Job.new(params.require(:job).permit(:title, :company, :pay, :location, :description))
+  	if @job.save
+  	 redirect_to jobs_path
+  	else
+  	 render "new"
+  	end
+  end
+
+  def show
+    @job = Job.find(params[:id])
+  end
+
+  def edit
+    @job = Job.find(params[:id])
+  end
+
+  def update
+    @job = Job.find(params[:id])
+    if @job.update(params.require(:job).permit(:title, :company, :pay, :location, :description))
+      flash[:notice] = "Successfully update job!"
+      redirect_to jobs_path
+    else
+      flash[:notice] = "Error updating job!"
+      render 'edit'
     end
+  end
 
-    def new
-    	@job = Job.new
-    end
-
-    def create
-    	@job = Job.new(params.require(:job).permit(:title, :company, :pay, :location, :description))
-    	if @job.save
-    		redirect_to jobs_path
-    	else
-    		render "new"
-    	end
-    end
-
-    def show
-      @job = Job.find(params[:id])
-    end
-
-    def edit
-      @job = Job.find(params[:id])
-    end
-
-    def update
-      @job = Job.find(params[:id])
-      if @job.update(params.require(:job).permit(:title, :company, :pay, :location, :description))
-         flash[:notice] = "Successfully update job!"
-        redirect_to jobs_path
-      else
-         flash[:notice] = "Error updating job!"
-        render 'edit'
-      end
-    end
-
-    def archive
-      def archive        
-      update_attribute!(:displayed, false)
-    end 
+  def archive
+    update_attribute!(:displayed, false)
+  end 
 
 
-    private
+  private
 
-    def jobs_params
-      params.require(:title).permit(:title, :company, :pay, :description)
-    end
+  def jobs_params
+    params.require(:title).permit(:title, :company, :pay, :description)
+  end
 
-    def find_jobs
-      @job = Jobs.find(params[:id])
-    end
+  def find_jobs
+    @job = Jobs.find(params[:id])
   end
 end
--- app/controllers/pages_controller.rb
+++ app/controllers/pages_controller.rb
@@ -2,32 +2,42 @@ class PagesController < ApplicationController
   
 
   def index
+
   end
 
   def about
+
   end
 
   def contact
+
   end
 
   def customers
+
   end
 
   def info_sys
+
   end
 
   def languages
+
   end
 
   def medical
+
   end
 
   def solutions
+
   end
 
   def specialized_training
+
   end
 
   def staffing
+
   end
 end
